4f1f9f518edac4dd6c85450698f051b0b6372e8c
Kovalchuk-Alexandr <mo250568kaa@gmail.com>
2023-11-28 12:33:25 +0200

    elegance fashion release

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-icon-wrapper").onclick = function () {
+    document.querySelector(".menu-icon").classList.toggle("menu-icon-active");
+    document
+        .querySelector(".nav__list-holder")
+        .classList.toggle("nav__list-holder--visible");
+    document
+        .querySelector("body")
+        .classList.toggle("menu-active");
+};
